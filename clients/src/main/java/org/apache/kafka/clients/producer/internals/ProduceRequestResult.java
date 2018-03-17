57978796804c18aa4a194f861b36a75ab96e76f4
pengzhe <pengzhepost@sina.com>
2018-03-17 22:27:28 +0800

    RecordBatch

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/ProduceRequestResult.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/ProduceRequestResult.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,21 +16,33 @@
  */
 package org.apache.kafka.clients.producer.internals;
 
+import org.apache.kafka.common.TopicPartition;
+
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.kafka.common.TopicPartition;
 
 
 /**
  * A class that models the future completion of a produce request for a single partition. There is one of these per
  * partition in a produce request and it is shared by all the {@link RecordMetadata} instances that are batched together
  * for the same partition in the request.
+ * <p>
+ * ProduceRequestResult类并未实现java.util.concurrent.Future接口
+ * 但是其包含了一个count值为1的CountDownLatch对象,实现了类似Future的功能
+ * <p>
+ * 当RecordBatch中的全部消息被正常响,或超时,或关闭生产生产者时,会调用ProduceRequestResult.done()方法
+ * 将produceFuture标记为完成,并通过ProduceRequestResult.error字段区分"异常完成"还是"正常完成",之后调用
+ * CountDownLatch.countDown()方法,此时会唤醒阻塞在CountDownLatch对象的await()方法的线程(这些线程通过
+ * ProduceRequestResult对象的await()方法等待上述三个事件的发生)
  */
 public final class ProduceRequestResult {
 
     private final CountDownLatch latch = new CountDownLatch(1);
     private volatile TopicPartition topicPartition;
+    /**
+     * baseOffset表示的事服务端为此RecordBatch种第一条消息分配的offset
+     * 这样每个消息根据此offset以及自身在此RecordBatch的相对偏移量就可以计算出其在服务端分区中的偏移量了
+     */
     private volatile long baseOffset = -1L;
     private volatile RuntimeException error;
 
@@ -39,9 +51,10 @@ public final class ProduceRequestResult {
 
     /**
      * Mark this request as complete and unblock any threads waiting on its completion.
+     *
      * @param topicPartition The topic and partition to which this record set was sent was sent
-     * @param baseOffset The base offset assigned to the record
-     * @param error The error that occurred if there was one, or null.
+     * @param baseOffset     The base offset assigned to the record
+     * @param error          The error that occurred if there was one, or null.
      */
     public void done(TopicPartition topicPartition, long baseOffset, RuntimeException error) {
         this.topicPartition = topicPartition;
@@ -59,8 +72,9 @@ public final class ProduceRequestResult {
 
     /**
      * Await the completion of this request (up to the given time interval)
+     *
      * @param timeout The maximum time to wait
-     * @param unit The unit for the max time
+     * @param unit    The unit for the max time
      * @return true if the request completed, false if we timed out
      */
     public boolean await(long timeout, TimeUnit unit) throws InterruptedException {
